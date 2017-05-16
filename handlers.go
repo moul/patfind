ff22645a682c89ac2af6ee9df92044eab61c4245
Manfred Touron <m@42.am>
2017-05-16 21:53:45 +0200

    Test famous patterns

--- handlers.go
+++ handlers.go
@@ -2,21 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Handler func(string) (float64, string)
 
 var EnabledHandlers = []Handler{
-	UniqueSymbols,
-	RepetitivePatterns,
-	// FIXME: check for famous patterns, 4242, 666
+	UniqueSymbolsScore,
+	RepetitivePatternsScore,
+	FamousPatternsScore,
 	// FIXME: check for suites
 	// FIXME: longest ending similar numbers (/000$/)
 	// FIXME: finger-typing-distance
 	// FIXME: longest repetitive symbol
 }
 
-func UniqueSymbols(n string) (score float64, comment string) {
+var FamousPatterns = []string{
+	"42",
+	"1337",
+	"8173",
+}
+
+func FamousPatternsScore(n string) (score float64, comment string) {
+	matchedPatterns := []string{}
+	for _, pattern := range FamousPatterns {
+		if strings.Contains(n, pattern) {
+			matchedPatterns = append(matchedPatterns, pattern)
+			score += 0.25
+		}
+		if strings.HasSuffix(n, pattern) {
+			score += 0.5
+		}
+	}
+
+	if score > 1 {
+		score = 1
+	}
+	if score > 0 {
+		comment = fmt.Sprintf("famous patterns: %s", strings.Join(matchedPatterns, ","))
+	}
+	return
+}
+
+func UniqueSymbolsScore(n string) (score float64, comment string) {
 	uniq := StringUniqueSymbols(n)
 	comment = fmt.Sprintf("%d unique symbols", len(uniq))
 	uniqueness := float64(len(n)) / (float64(len(uniq)) / (float64(len(n)))) / 100 // average percentage of presence per symbol
@@ -25,7 +53,7 @@ func UniqueSymbols(n string) (score float64, comment string) {
 	return
 }
 
-func RepetitivePatterns(n string) (score float64, comment string) {
+func RepetitivePatternsScore(n string) (score float64, comment string) {
 	var (
 		bestMatchedLen int
 		// FIXME: count the number of working "i"
--- handlers_test.go
+++ handlers_test.go
@@ -6,59 +6,100 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestUniqueSymbols(t *testing.T) {
-	Convey("UniqueSymbols", t, FailureContinues, func() {
+func TestUniqueSymbolsScore(t *testing.T) {
+	Convey("UniqueSymbolsScore", t, FailureContinues, func() {
 		var (
 			score   float64
 			comment string
 		)
 
-		score, comment = UniqueSymbols("0000000000")
+		score, comment = UniqueSymbolsScore("0000000000")
 		So(score, ShouldEqual, float64(1))
 		So(comment, ShouldEqual, "1 unique symbols")
 
-		score, comment = UniqueSymbols("0707070707")
+		score, comment = UniqueSymbolsScore("0707070707")
 		So(score, ShouldEqual, float64(0.5))
 		So(comment, ShouldEqual, "2 unique symbols")
 
-		score, comment = UniqueSymbols("0123456789")
+		score, comment = UniqueSymbolsScore("0123456789")
 		So(score, ShouldEqual, float64(0.1))
 		So(comment, ShouldEqual, "10 unique symbols")
 	})
 }
 
-func TestRepetitivePatterns(t *testing.T) {
-	Convey("RepetitivePatterns", t, FailureContinues, func() {
+func TestFamousPatternsScore(t *testing.T) {
+	Convey("FamousPatternsScore", t, FailureContinues, func() {
 		var (
 			score   float64
 			comment string
 		)
 
-		score, comment = RepetitivePatterns("0000000000")
+		score, comment = FamousPatternsScore("0606060606")
+		So(score, ShouldEqual, float64(0))
+		So(comment, ShouldEqual, "")
+
+		score, comment = FamousPatternsScore("0619181742")
+		So(score, ShouldEqual, float64(0.75))
+		So(comment, ShouldEqual, "famous patterns: 42")
+
+		score, comment = FamousPatternsScore("0619181337")
+		So(score, ShouldEqual, float64(0.75))
+		So(comment, ShouldEqual, "famous patterns: 1337")
+
+		score, comment = FamousPatternsScore("0619184200")
+		So(score, ShouldEqual, float64(0.25))
+		So(comment, ShouldEqual, "famous patterns: 42")
+
+		score, comment = FamousPatternsScore("0619133700")
+		So(score, ShouldEqual, float64(0.25))
+		So(comment, ShouldEqual, "famous patterns: 1337")
+
+		score, comment = FamousPatternsScore("0642421234")
+		So(score, ShouldEqual, float64(0.25))
+		So(comment, ShouldEqual, "famous patterns: 42")
+
+		score, comment = FamousPatternsScore("0642133712")
+		So(score, ShouldEqual, float64(0.5))
+		So(comment, ShouldEqual, "famous patterns: 42,1337")
+
+		score, comment = FamousPatternsScore("0642421337")
+		So(score, ShouldEqual, float64(1))
+		So(comment, ShouldEqual, "famous patterns: 42,1337")
+	})
+}
+
+func TestRepetitivePatternsScore(t *testing.T) {
+	Convey("RepetitivePatternsScore", t, FailureContinues, func() {
+		var (
+			score   float64
+			comment string
+		)
+
+		score, comment = RepetitivePatternsScore("0000000000")
 		So(score, ShouldEqual, float64(1))
 		So(comment, ShouldEqual, "2 repetitions of 5 symbols")
 
-		score, comment = RepetitivePatterns("0707070707")
+		score, comment = RepetitivePatternsScore("0707070707")
 		So(score, ShouldEqual, float64(1))
 		So(comment, ShouldEqual, "5 repetitions of 2 symbols")
 
-		score, comment = RepetitivePatterns("0682824242")
+		score, comment = RepetitivePatternsScore("0682824242")
 		So(score, ShouldEqual, 0.8)
 		So(comment, ShouldEqual, "4 repetitions of 2 symbols")
 
-		score, comment = RepetitivePatterns("0612341234")
+		score, comment = RepetitivePatternsScore("0612341234")
 		So(score, ShouldEqual, 0.8)
 		So(comment, ShouldEqual, "2 repetitions of 4 symbols")
 
-		score, comment = RepetitivePatterns("0612344242")
+		score, comment = RepetitivePatternsScore("0612344242")
 		So(score, ShouldEqual, 0.4)
 		So(comment, ShouldEqual, "2 repetitions of 2 symbols")
 
-		score, comment = RepetitivePatterns("0612345666")
+		score, comment = RepetitivePatternsScore("0612345666")
 		So(score, ShouldEqual, 0)
 		So(comment, ShouldEqual, "")
 
-		score, comment = RepetitivePatterns("0123456789")
+		score, comment = RepetitivePatternsScore("0123456789")
 		So(score, ShouldEqual, float64(0))
 		So(comment, ShouldEqual, "")
 	})
